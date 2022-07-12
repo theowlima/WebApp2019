80ef4ef1cf95150cf593504f83eeb59ea07ffaae
Theo Lima <theomadeira83@gmail.com>
2022-07-12 16:14:35 -0300

    CRUD Scaffolding

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
